bb70494bcfe4a86114e88090cae531bea67f736b
Olha <oyashnaya88@gmail.com>
2024-09-02 23:52:11 +0300

    markup

--- js/task-2.js
+++ js/task-2.js
@@ -1,13 +1,11 @@
 function makeArray(firstArray, secondArray, maxLength) {
-  
-    const newArray = firstArray.concat(secondArray);
-    if(newArray.length > maxLength) {
+  const newArray = firstArray.concat(secondArray);
+  if (newArray.length > maxLength) {
     return newArray.slice(0, maxLength);
-    }
-       else {
-        return newArray;
-       }
-    }
+  } else {
+    return newArray;
+  }
+}
 
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
